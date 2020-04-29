7075d2703e68a2aa9cebceb64ec7aea30a6d068b
jussiholtta <jussi.holtta@gmail.com>
2020-04-29 22:37:11 +0300

    started working on anyrandomizer again, deleting items borken

--- src/anyrandomizer/anyrandomizer.spec.js
+++ src/anyrandomizer/anyrandomizer.spec.js
@@ -1,22 +1,40 @@
-import Anyrandomizer from './anyrandomizer.js';
+import {Anyrandomizer} from './anyrandomizer.js';
 
 describe('Main view tests', ()=>{
-  let anyrand;
-
-  beforeEach(() => {
-    document.body.innerHTML ='<div id="Anyrandomizer"></div>';
-    anyrand = new Anyrandomizer(document.getElementById('Anyrandomizer'));
-  });
-  afterEach(() => {
-    jest.resetModules();
-  });
+  const anyrand = new Anyrandomizer(document.body);
 
   it('should exist', ()=>{
     expect(anyrand).not.toBe(undefined);
   });
 
-  it('should have div(s) for items', ()=>{
-    const div = document.getElementById('Anyrandomizer');
-    expect(div).toEqual(expect.any(HTMLElement));
+  it('should have field for text', ()=>{
+    const items = document.getElementById('items');
+    expect(items).toEqual(expect.any(HTMLInputElement));
+    expect(items.type).toEqual('text');
+    expect(items.value).toEqual('a,b,c');
+  });
+
+  it('should display textfield contents as divs', ()=>{
+    const itemDivs = document.getElementsByClassName('item');
+    expect(itemDivs.length).toEqual(3);
+  });
+
+  it('should be able to delete item divs', ()=>{
+    anyrand.deleteItems();
+    const itemDivs = document.getElementsByClassName('item');
+    expect(itemDivs.length).toEqual(0);
+  });
+
+  it('should have a randomize button', ()=>{
+    const button = document.getElementById('button');
+    expect(button).toEqual(expect.any(HTMLButtonElement));
+    expect(button.innerHTML).toEqual('Refresh');
+  });
+
+  it('should shuffle items when button is clicked', ()=>{
+    const picture1 = document.getElementById('svgContainer').innerHTML;
+    anyrand.refreshClicked();
+    const picture2 = document.getElementById('svgContainer').innerHTML;
+    expect(picture1).not.toEqual(picture2);
   });
 });
