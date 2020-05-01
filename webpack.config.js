cc6fa6ce4757fa0be382f5c8e2bd9e89a94d720e
jussiholtta <jussi.holtta@gmail.com>
2020-05-01 11:44:00 +0300

    asddsfa

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,22 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+function recursiveIssuer(m) {
+  if(m.issuer) {
+    return recursiveIssuer(m.issuer);
+  }
+  if(m.name) {
+    return m.name;
+  }
+  return false
+}
 
 module.exports = {
 	entry: {
 		'timer': './babeltmp/timer.js',
 		'anyrandomizer': './babeltmp/anyrandomizer.js',
+		'anyrandomizercss': './src/anyrandomizer/anyrandomizer.css',
 		'randompicture': './babeltmp/randompicture.js',
 		'wtf': './babeltmp/wtf.js',
 	},
@@ -12,7 +24,23 @@ module.exports = {
 		path: path.resolve(__dirname, 'dist'),
 		filename: '[name]/[name].js'
 	},
+	optimization: {
+		splitChunks: {
+			cacheGroups: {
+        criticalStyles: {
+  				name: "anyrandomizercss",
+	  			test: (m, c, entry = "anyrandomizercss") =>
+		  			m.constructor.name === "CssModule" && recursiveIssuer(m) === entry, 
+			  	chunks: "all",
+  				enforce: true
+        }
+			}
+		}
+	},
 	plugins: [
+		new MiniCssExtractPlugin({
+			filename: "[name].css"
+		}),
 		new HtmlWebpackPlugin({
 		inject: false,
 		filename: './wtf/index.html',
@@ -29,7 +57,7 @@ module.exports = {
 		inject: false,
 		filename: './anyrandomizer/index.html',
 		template: './src/anyrandomizer/index.html',
-		chunks: ['anyrandomizer']
+		chunks: ['anyrandomizer','anyrandomizercss']
 		}),
 		new HtmlWebpackPlugin({
 		inject: false,
@@ -38,5 +66,23 @@ module.exports = {
 		chunks: ['randompicture']
 		})
 	],
+	module: {
+		rules: [	
+			{
+			test: /\.css$/,
+			use: [
+				MiniCssExtractPlugin.loader,
+				{
+					loader: "css-loader",
+					options: {
+						modules: {
+  						localIdentName: "[name]__[local]",
+            },
+					}
+				},
+			],
+			},
+		],
+	},
 	mode: 'production'
 };
